19c14f93e1a1996c810fd810f69e4231528c8e2c
Pramod_Acharya <pramodacharya808@gmail.com>
2024-04-30 14:15:00 +0530

    initial commit

---
+++ src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <div className="text-3xl capitalize font-Jersey">
+      AI Image <span className="text-3xl">Generator</span>
+    </div>
+  );
+}
+
+export default Heading;
